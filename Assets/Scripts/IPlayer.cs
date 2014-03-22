9032a47a943e81d077dd73643bb68b56b7ab1a44
Brieuc Swales - Laptop <brieucswales@gmail.com>
2014-03-22 10:52:57 +0100

    - Classe Enemy - Interface Player - Classe Player - Classe Tile

---
+++ Assets/Scripts/IPlayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public interface IPlayer {
+
+	int Life {
+		get;
+		set;
+	}
+
+	float Speed {
+		get;
+		set;
+	}
+
+	/*
+	List<T> Gems {
+
+	}
+	*/
+
+	void Move();
+
+	void Attack();
+
+	void Die();
+
+}
